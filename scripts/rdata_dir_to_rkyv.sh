8b2b2b719c20c985824d29b67adad4a69079f515
mrvillage <josef@mrvillage.dev>
2024-06-13 16:42:16 -0400

    Check argument count

--- scripts/rdata_dir_to_rkyv.sh
+++ scripts/rdata_dir_to_rkyv.sh
@@ -2,6 +2,11 @@
 dir=$1
 jobs=$2
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: rdata_dir_to_rkyv.sh <dir> <jobs>"
+    exit 1
+fi
+
 cd $( dirname -- "${BASH_SOURCE[0]}" )
 
 find $dir -type f | parallel -j $jobs Rscript ./df_to_rkyv.r
