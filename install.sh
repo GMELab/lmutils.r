28c967c71ba0752d257695faff17c96882703694
mrvillage <josef@mrvillage.dev>
2024-11-13 12:32:24 -0500

    Update install stuff

---
+++ install.sh
@@ -0,0 +1,16 @@
+export TMPDIR=$(mktemp -d --tmpdir=$(pwd))
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+unset TMPDIR
+if [ -f $HOME/.cargo/env ]; then
+    echo "LMUTILS:: Rust is installed"
+else
+    echo "LMUTILS:: Rust is not installed"
+fi
+
+R -e "install.packages('https://github.com/GMELab/lmutils.r/releases/download/latest/lmutils.tar.gz', repos = NULL)" > /dev/null
+if [ $? -eq 0 ]; then
+    echo "LMUTILS:: lmutils is installed"
+else
+    echo "LMUTILS:: lmutils is not installed"
+fi
