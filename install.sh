2bfe98f39ee0b05898aa9ddb4a738abad8b29645
mrvillage <josef@mrvillage.dev>
2025-02-27 15:42:29 -0500

    Update

--- install.sh
+++ install.sh
@@ -1,29 +1,12 @@
-# if -v is passed, then verbose mode is enabled (set a variable to print commands)
-verbose=0
-while getopts 'v' flag; do
-    case "${flag}" in
-        v) verbose=1 ;;
-        *) error "Unexpected option ${flag}" ;;
-    esac
-done
-
 export TMPDIR=$(mktemp -d --tmpdir=$(pwd))
 # install rust if not installed
 if [ -f $HOME/.cargo/env ]; then
     echo "LMUTILS:: Rust is already installed"
-    if [ $verbose -eq 1 ]; then
-        rustup update
-    else
-        rustup update > /dev/null
-    fi
+    rustup update > /dev/null
     echo "LMUTILS:: Rust is updated"
 else
     echo "LMUTILS:: Rust not found, installing"
-    if [ $verbose -eq 1 ]; then
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    else
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y > /dev/null
-    fi
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y > /dev/null
     source $HOME/.cargo/env
     if [ -f $HOME/.cargo/env ]; then
         echo "LMUTILS:: Rust is now installed"
@@ -40,11 +23,7 @@ echo "LMUTILS:: Installing lmutils"
 if [ ! -d $HOME/.lmutils ]; then
     mkdir $HOME/.lmutils
 fi
-if [ $verbose -eq 1 ]; then
-    R -e "install.packages('https://github.com/GMELab/lmutils.r/releases/latest/download/lmutils.r.tar.gz', repos = NULL, lib = '$HOME/.lmutils')"
-else
-    R -e "install.packages('https://github.com/GMELab/lmutils.r/releases/latest/download/lmutils.r.tar.gz', repos = NULL, lib = '$HOME/.lmutils')" > /dev/null
-fi
+R -e "install.packages('https://github.com/GMELab/lmutils.r/releases/latest/download/lmutils.r.tar.gz', repos = NULL, lib = '$HOME/.lmutils')" > /dev/null
 if [ $? -eq 0 ]; then
     echo "LMUTILS:: lmutils is now installed"
 else
